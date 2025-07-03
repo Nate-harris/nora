d6e5b6df5356bf32ddc76d281db70078550aed39
Nate-harris <ngharris91@gmail.com>
2025-07-03 18:23:54 -0400

    Update WoodgrainShaderSketch.js

--- components/WoodgrainShaderSketch.js
+++ components/WoodgrainShaderSketch.js
@@ -49,14 +49,11 @@ const WoodgrainShaderSketch = ({
     );
   }
 
-  // --- FIX: Don't call .parent if ref is missing ---
-  const setup = (p5, canvasParentRef) => {
+  // DO NOT call .parent(canvasParentRef)
+  const setup = (p5 /*, canvasParentRef */) => {
     p5.pixelDensity(1);
     p5.setAttributes("alpha", true);
     canvasRef.current = p5.createCanvas(width, height, p5.WEBGL);
-    if (canvasParentRef) {
-      canvasRef.current.parent(canvasParentRef);
-    }
     p5.noStroke();
     shaderTexture.current = p5.createGraphics(width, height, p5.WEBGL);
     shaderTexture.current.noStroke();
